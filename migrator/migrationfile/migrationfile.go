602a699ebdd75f01655156b0a637160bf37e7cdb
David Ko <david.ko@velvetreactor.com>
2020-05-09 22:22:52 -0400

    readFile function

--- migrator/migrationfile/migrationfile.go
+++ migrator/migrationfile/migrationfile.go
@@ -7,12 +7,18 @@ import (
 	"os"
 )
 
-type Migration struct {
+type MigrationFile struct {
 	Action     string                 `json:"action"`
 	Parameters map[string]interface{} `json:"parameters"`
 }
 
 func Migrate(file string) {
+	migrationFile := readFile(file)
+
+	fmt.Println(migrationFile.Parameters["columns"])
+}
+
+func readFile(file string) *MigrationFile {
 	filePath := fmt.Sprintf("db/migrations/%s", file)
 
 	fileIO, e := os.Open(filePath)
@@ -20,11 +26,11 @@ func Migrate(file string) {
 		log.Fatal(e)
 	}
 
-	var migration Migration
+	var migration MigrationFile
 	e = json.NewDecoder(fileIO).Decode(&migration)
 	if e != nil {
 		log.Fatal(e)
 	}
 
-	fmt.Println(migration.Parameters["columns"])
+	return &migration
 }
