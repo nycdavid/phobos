10c40581cc92bf204cc024eb63ab9a94ab7cd7c5
David Ko <david.ko@velvetreactor.com>
2020-05-15 21:04:25 -0400

    Adds some front end components

--- main.go
+++ main.go
@@ -1,15 +1,22 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
+	r.LoadHTMLGlob("views/*")
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	r.GET("/", Root)
 
 	r.Run()
+}
+
+func Root(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.tmpl", gin.H{
+		"title": "Index",
+	})
 }
