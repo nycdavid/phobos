0fe6909acbc48df4ad90278de83ca661bb4dbce6
David Ko <david.ko@velvetreactor.com>
2020-05-23 19:48:29 -0400

    Refactor into React methods

--- webpack/src/new_card_modal.js
+++ webpack/src/new_card_modal.js
@@ -26,6 +26,76 @@ class NewCardModal extends React.Component {
     this.setState({ tabGroups: tabGroups });
   }
 
+  frontSection() {
+    return (
+      <div className="card-front">
+        <ul className="nav nav-tabs">
+          <li className="nav-item">
+            <a
+              className={`nav-link ${this.activeClass("front", 0)}`}
+              href="#"
+              onClick={() => this.changeTab("front", 0)}
+            >
+              Edit
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              className={`nav-link ${this.activeClass("front", 1)}`}
+              href="#"
+              onClick={() => this.changeTab("front", 1)}
+            >
+              Preview
+            </a>
+          </li>
+        </ul>
+        <form>
+          <div className="form-group">
+            <label htmlFor="" className="col-form-label">
+              Front
+            </label>
+            <textarea type="text" className="form-control" />
+          </div>
+        </form>
+      </div>
+    );
+  }
+
+  backSection() {
+    return (
+      <div className="card-back">
+        <ul className="nav nav-tabs">
+          <li className="nav-item">
+            <a
+              className={`nav-link ${this.activeClass("back", 0)}`}
+              href="#"
+              onClick={() => this.changeTab("back", 0)}
+            >
+              Edit
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              className={`nav-link ${this.activeClass("back", 1)}`}
+              href="#"
+              onClick={() => this.changeTab("back", 1)}
+            >
+              Preview
+            </a>
+          </li>
+        </ul>
+        <form>
+          <div className="form-group">
+            <label htmlFor="" className="col-form-label">
+              Back
+            </label>
+            <textarea type="text" className="form-control" />
+          </div>
+        </form>
+      </div>
+    );
+  }
+
   render() {
     return (
       <div
@@ -40,34 +110,8 @@ class NewCardModal extends React.Component {
               <h5>New card</h5>
             </div>
             <div className="modal-body">
-              <ul className="nav nav-tabs">
-                <li className="nav-item">
-                  <a
-                    className={`nav-link ${this.activeClass("front", 0)}`}
-                    href="#"
-                    onClick={() => this.changeTab("front", 0)}
-                  >
-                    Edit
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a
-                    className={`nav-link ${this.activeClass("front", 1)}`}
-                    href="#"
-                    onClick={() => this.changeTab("front", 1)}
-                  >
-                    Preview
-                  </a>
-                </li>
-              </ul>
-              <form>
-                <div className="form-group">
-                  <label htmlFor="" className="col-form-label">
-                    Front
-                  </label>
-                  <textarea type="text" className="form-control" />
-                </div>
-              </form>
+              {this.frontSection()}
+              {this.backSection()}
             </div>
           </div>
         </div>
