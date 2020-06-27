fe484740eea8ff77029b22cbbfb315650bb18311
David Ko <david.ko@velvetreactor.com>
2020-06-27 07:01:58 -0400

    Adds Typescript load support to Webpack

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -12,10 +12,15 @@ module.exports = {
         exclude: /node_modules/,
         use: ["babel-loader"],
       },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ["ts-loader"],
+      },
     ],
   },
   resolve: {
-    extensions: ["*", ".js", ".jsx"],
+    extensions: ["*", ".js", ".jsx", ".ts", ".tsx"],
   },
   output: {
     filename: "[name].js",
