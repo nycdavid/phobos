eeb92cf31aa6056df579bcbc07779ed41d1c2be6
David Ko <david.ko@velvetreactor.com>
2020-08-11 07:32:26 -0400

    Adds docker compose file

--- migrator/cmd/dbcreate_test.go
+++ migrator/cmd/dbcreate_test.go
@@ -1,7 +1,10 @@
 package cmd
 
 import (
+	"database/sql"
 	"testing"
+
+	_ "github.com/lib/pq"
 )
 
 func TestDbCreate_UsesDevelopmentByDefault(t *testing.T) {
@@ -19,5 +22,21 @@ func TestDbCreate_ConfigFileInCtx(t *testing.T) {
 
 	if configFile == nil {
 		t.Errorf("Expected non-nil config file")
+	}
+}
+
+func TestDbCreate_CreatesDatabase(t *testing.T) {
+	dbo, e := sql.Open(
+		"postgres",
+		"host=localhost port=5432 database=test password=password user=postgres database=test sslmode=disable",
+	)
+	if e != nil {
+		t.Error(e)
+	}
+
+	e = dbo.Ping()
+
+	if e != nil {
+		t.Error(e)
 	}
 }
